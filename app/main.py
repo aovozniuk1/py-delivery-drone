db8673b5379b4e6229aaa8fb3bc191e0f8ac858a
avozniuk <a.o.vozniuk@gmail.com>
2025-10-02 19:52:23 +0300

    updates

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight
-                   : int, coords: list = None) -> None:
+                 : int, coords: list = None) -> None:
         if coords is None:
             super().__init__(name, weight, [0, 0, 0])
         else:
